71acc155d0af0082303e65a9653c9c9220a58398
SrhJerry <srhjerry@gmail.com>
2018-07-04 14:02:30 +0800

    Add the gallery page contents

---
+++ resources/js/galScript.js
@@ -0,0 +1,35 @@
+'use strict'
+$(document).ready(function() {
+    
+    /* Animations on scroll */
+    $('.galpho-js-wp-1').waypoint(function(direction) {
+        $('.galpho-js-wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '500%'
+    });
+    
+    $('.galpho-js-wp-2').waypoint(function(direction) {
+        $('.galpho-js-wp-2').addClass('animated fadeIn');
+    }, {
+
+    });
+    
+    $('.galpho-js-wp-3').waypoint(function(direction) {
+        $('.galpho-js-wp-3').addClass('animated fadeIn');
+    }, {
+
+    });
+    
+    $('.galpho-js-wp-4').waypoint(function(direction) {
+        $('.galpho-js-wp-4').addClass('animated fadeIn');
+    }, {
+
+    });
+    
+    $('.galpho-js-wp-5').waypoint(function(direction) {
+        $('.galpho-js-wp-5').addClass('animated fadeIn');
+    }, {
+
+    });
+    
+});
