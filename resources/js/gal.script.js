005510156ced99b09f89c36c7ead8cfcc9ca0164
SrhJerry <srhjerry@gmail.com>
2018-08-06 17:52:00 +0800

    Add preload feature

--- resources/js/gal.script.js
+++ resources/js/gal.script.js
@@ -55,28 +55,46 @@ function showSlides(n) {
   var dots = document.getElementsByClassName("demo");
   var show = document.getElementById("slideShow");
 
-  if (n > 84) {slideIndex = 1}
-  else if (n < 1) {slideIndex = 84}
+  if (n > 86) {slideIndex = 1}
+  else if (n < 1) {slideIndex = 86}
   else {slideIndex = n}
 
-  var photosrc;
-  if(slideIndex>0&&slideIndex<=10){photosrc = "/resources/img/gallery/A/"+(slideIndex)+".jpg";}
-  else if(slideIndex>10&&slideIndex<=18){photosrc = "/resources/img/gallery/B/"+(slideIndex)+".jpg";}
-  else if(slideIndex>18&&slideIndex<=36||slideIndex==83||slideIndex==84){photosrc = "/resources/img/gallery/C/"+(slideIndex)+".jpg";}
-  else if(slideIndex>36&&slideIndex<=45){photosrc = "/resources/img/gallery/D/"+(slideIndex)+".jpg";}
-  else if(slideIndex>45&&slideIndex<=62){photosrc = "/resources/img/gallery/E/"+(slideIndex)+".jpg";}
-  else if(slideIndex>62&&slideIndex<=72){photosrc = "/resources/img/gallery/F/"+(slideIndex)+".jpg";}
-  else if(slideIndex>72&&slideIndex<=82){photosrc = "/resources/img/gallery/G/"+(slideIndex)+".jpg";}
-  else {photosrc = "/resources/img/gallery/thumbnail/"+(slideIndex)+".jpg";}
+  
     
   
-  show.src = photosrc;
+  show.src = getPhotoSRC(slideIndex);
   slides[0].style.display = "block";
   
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
-      dots[i].src = "/resources/img/gallery/thumbnail/"+((i+slideIndex-4+84)%84+1)+".jpg";
+      dots[i].src = "/resources/img/gallery/thumbnail/"+((i+slideIndex-4+86)%86+1)+".jpg";
   }
     
   dots[slideIndex-1].className += " active";
 }
+
+function getPhotoSRC(slideIndex){
+    let photosrc;
+  
+    if(slideIndex>0&&slideIndex<=10){photosrc = "/resources/img/gallery/A/"+(slideIndex)+".jpg";}
+    else if(slideIndex>10&&slideIndex<=18){photosrc = "/resources/img/gallery/B/"+(slideIndex)+".jpg";}
+    else if(slideIndex>18&&slideIndex<=36||slideIndex==83||slideIndex==84){photosrc = "/resources/img/gallery/C/"+(slideIndex)+".jpg";}
+    else if(slideIndex>36&&slideIndex<=45){photosrc = "/resources/img/gallery/D/"+(slideIndex)+".jpg";}
+    else if(slideIndex>45&&slideIndex<=62){photosrc = "/resources/img/gallery/E/"+(slideIndex)+".jpg";}
+    else if(slideIndex>62&&slideIndex<=72||slideIndex==85||slideIndex==86){photosrc = "/resources/img/gallery/F/"+(slideIndex)+".jpg";}
+    else if(slideIndex>72&&slideIndex<=82){photosrc = "/resources/img/gallery/G/"+(slideIndex)+".jpg";}
+    else {photosrc = "/resources/img/gallery/thumbnail/"+(slideIndex)+".jpg";}
+    
+    return photosrc;
+}
+
+window.onload = function() {
+	setTimeout(function() {
+		// preload image
+        let i;
+        
+        for(i=1; i<87; i++){
+		  new Image().src = getPhotoSRC(i);
+        }
+	}, 5000);
+};
