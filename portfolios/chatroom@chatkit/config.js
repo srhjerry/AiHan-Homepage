0d82a3457c3451782b67c5bbae94510ffb70de93
SrhJerry <srhjerry@gmail.com>
2018-09-14 13:49:11 +0800

    Demo page 1/e completed.

---
+++ portfolios/chatroom@chatkit/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0b695fb6-ffec-4e03-8d19-5e30e0bdcd95/token";
+const instanceLocator = "v1:us1:0b695fb6-ffec-4e03-8d19-5e30e0bdcd95";
+
+export { tokenUrl, instanceLocator }
